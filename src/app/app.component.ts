552a053e0297163bf43bc369a6231b4cc23da5a7
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 10:47:05 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  isApproved = true;
 
-
+  approve(status) {
+    this.isApproved = status;
+  }
 }
