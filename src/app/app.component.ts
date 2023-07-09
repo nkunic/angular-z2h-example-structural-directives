62e2ede0c913dacfef6f3ed1df44585c83826242
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 11:11:07 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  // ngIf
   isApproved = true;
 
   approve(status) {
     this.isApproved = status;
   }
+
+  //ngFor
+  numbers = [
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+    'Ten',
+
+  ]
+
 }
